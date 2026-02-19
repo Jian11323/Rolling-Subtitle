27475ed88d9a863547914c8828e73edacb0c995b
crazy786781 <mazhiyuan401@163.com>
2026-02-19 15:31:17 +0800

    Add Chinese translation to LICENSE, update README, add helper script

--- git_commit_license_and_push.bat
+++ git_commit_license_and_push.bat
@@ -1,10 +1,11 @@
 @echo off
 cd /d "%~dp0"
 
-git add LICENSE
-git commit -m "Add Chinese translation to LICENSE"
+git add LICENSE README.md git_commit_license_and_push.bat
+git status --short
+git commit -m "Add Chinese translation to LICENSE, update README, add helper script"
 if errorlevel 1 (
-    echo Commit failed. Maybe nothing to commit or not a git repo.
+    echo Commit failed. Check above. If "nothing to commit", all changes may already be committed.
     pause
     exit /b 1
 )
